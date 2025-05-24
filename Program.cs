6f9c38b9d20dfc9821fb5a82e875cf890884eeeb
Guilherme Utida De Jeses <guiutida@gmail.com>
2025-05-24 17:02:50 -0300

    comeco

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int quantidade, numero = 0;
+
+Console.WriteLine("--- Esperto Contra Sabido ---");
+
+Console.Write("Quantos alimentos serão distribuídos? ");
+
+  quantidade = Convert.ToInt32(Console.ReadLine());
+
+while (quantidade > 0 )
+{
+ 
+}
